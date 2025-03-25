b0858f656418b8caeb2a1861fe279cbd5d43dd6a
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2025-03-25 19:52:02 +0900

    ros package creation

---
+++ foundation_stereo_ros/foundation_stereo_node.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import rclpy
+from rclpy.node import node
+
+
+class FoundationStereo(Node):
+    def __init__(self, name: str = 'foundation_stereo'):
+        pass
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
